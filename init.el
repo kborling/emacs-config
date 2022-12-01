a9fbe2d9feccc4dca3b00de6b84339d0086fad43
Kevin Borling <kborling@protonmail.com>
2022-11-30 20:20:11 -0500

    Add OSX Keybindings

--- init.el
+++ init.el
@@ -1504,6 +1504,24 @@ The cursor becomes a blinking bar, per `kdb/cursor-type-mode'."
 
   :bind ("C-c m" . kdb/olivetti-mode))
 
+;; Macos ========================================== ;;
+
+(when (equal system-type 'darwin)
+  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier 'none)
+  ;; Make mouse wheel / trackpad scrolling less jerky
+  (setq mouse-wheel-scroll-amount '(1
+                                    ((shift) . 5)
+                                    ((control))))
+  (dolist (multiple '("" "double-" "triple-"))
+    (dolist (direction '("right" "left"))
+      (global-set-key (read-kbd-macro (concat "<" multiple "wheel-" direction ">")) 'ignore)))
+  (global-set-key (kbd "M-`") 'ns-next-frame)
+  (global-set-key (kbd "M-h") 'ns-do-hide-emacs)
+  (global-set-key (kbd "M-˙") 'ns-do-hide-others)
+  (global-set-key (kbd "M-ˍ") 'ns-do-hide-others) ;; what describe-key reports for cmd-option-h
+  )
+
 ;; Custom ========================================= ;;
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
