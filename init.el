fd8e1c8d05686f316257c88e8ce2f581deb7aab7
Kevin Borling <kborling@protonmail.com>
2023-04-19 21:22:48 -0400

    Add Org-Roam Package
    Add org-roam and org-roam-ui packages. Rebind some keys. Add checks to
    avoid install certain packages on windows (vterm, org-roam, org-roam-ui).

--- init.el
+++ init.el
@@ -179,6 +179,8 @@
   (define-key map (kbd "C-c C-r") #'query-replace)
 
   (define-key map (kbd "C-;") #'comment-line)
+  (define-key map (kbd "C-x f") #'project-find-file)
+  (define-key map (kbd "C-x /") #'set-fill-column)
 
   (define-key map (kbd "M-(") #'insert-pair)
   (define-key map (kbd "M-\"") #'insert-pair)
@@ -206,7 +208,7 @@
 ;; Theming ================================================ ;;
 (let ((font "Comic Code"))
   (set-face-attribute 'default nil
-                      :family font :weight 'regular :height 130)
+                      :family font :weight 'regular :height 140)
   (set-face-attribute 'bold nil
                       :family font :weight 'medium)
   (set-face-attribute 'italic nil
@@ -603,7 +605,9 @@
 
 ;; vterm ================================================ ;;
 
-(use-package vterm)
+(use-package vterm
+  :if (or (eq system-type 'gnu/linux)
+          (eq system-type 'darwin)))
 
 ;; Vertico ============================================= ;;
 
@@ -1257,6 +1261,42 @@
   (interactive)
   (find-file org-directory))
 (global-set-key (kbd "C-c e f") 'kdb-org-find-file)
+
+;; Org Roam ===================================== ;;
+
+(use-package org-roam
+  :if (or (eq system-type 'gnu/linux)
+          (eq system-type 'darwin))
+  :custom
+  (org-roam-directory (file-truename "~/roam"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
+
+(use-package org-roam-ui
+  :if (or (eq system-type 'gnu/linux)
+          (eq system-type 'darwin))
+  :elpaca (org-roam-ui :host github :repo "org-roam/org-roam-ui" :branch "main" :files ("*.el" "out"))
+  :after org-roam
+  ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+  ;;         a hookable mode anymore, you're advised to pick something yourself
+  ;;         if you don't care about startup time, use
+  ;;  :hook (after-init . org-roam-ui-mode)
+  :config
+  (setq org-roam-ui-sync-theme t
+        org-roam-ui-follow t
+        org-roam-ui-update-on-save t
+        org-roam-ui-open-on-start t))
 
 ;; Config ======================================= ;;
 
